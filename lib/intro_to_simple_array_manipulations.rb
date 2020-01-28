0bd5f5e8c3f914a94a8aabd80de7cf861a64eae0
Jiatao Lu <alohajt94@gmail.com>
2020-01-28 22:24:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,25 +1,25 @@
-def using_push(Array, String)
-  Array.push(String)
+def using_push(arr, str)
+  arr.push(str)
 end
 
-def using_unshift(Array, String)
-  Array.unshift(String)
+def using_unshift(str)
+  Array.unshift(str)
 end
  
-def using_pop(Array)
-  Array.pop
+def using_pop(arr)
+  arr.pop
 end
 
-def pop_with_args(Array)
-  Array.pop(2)
+def pop_with_args(arr)
+  arr.pop(2)
 end
 
-def using_shift(Array)
-  Array.shift
+def using_shift(arr)
+  arr.shift
 end
 
-def shift_with_args(Array)
-  Array.shift(2)
+def shift_with_args(arr)
+  arr.shift(2)
 end
 
 def using_concat(a, b)
